770cdd83fa41405f9e8dca7d6f9e33336842c10c
ANDREY <andrey.kiyas@gmail.com>
2022-12-21 13:15:34 +0200

    FindMaxSecondThirdInt

---
+++ src/FindMaxSecondThirdInt01.java
@@ -0,0 +1,22 @@
+public class FindMaxSecondThirdInt01 {
+    public static void main(String[] args) {
+
+        int[] mas = {12, 5, 8, 6, -12, 12, 10, 7, 6, 11, 3, 0, 10};
+        int max = 0, preMax = 0, prePreMax = 0;
+
+        for (int i = 0; i < mas.length; i++) {
+            if (mas[i] > max) {
+                max = mas[i];
+            }
+            if (mas[i] > preMax && mas[i] < max) {
+                preMax = mas[i];
+            }
+            if (mas[i] > prePreMax && mas[i] < preMax) {
+                prePreMax = mas[i];
+            }
+        }
+        System.out.println("Result max: " + max + "\n" +
+                "Result preMax: " + preMax + "\n" +
+                "Result prePreMax: " + prePreMax);
+    }
+}
---
+++ src/FindMaxSecondThirdInt02.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class FindMaxSecondThirdInt02 {
+    public static void main(String[] args) {
+        Integer[] mas = {12, 5, 8, 6, -12, 12, 10, 7, 6, 11, 3, 0, 10};
+        LinkedList<Integer> masResult = new LinkedList<>();
+        Arrays.sort(mas, Collections.reverseOrder());
+        int temp = 0;
+        for (int i = 0; i < mas.length; i++ ){
+            if(mas[i] != temp){
+                masResult.add(mas[i]);
+            }
+            temp = mas[i];
+        }
+        System.out.println("Result max: " + masResult.get(0) + "\n" +
+                "Result preMax: " + masResult.get(1) + "\n" +
+                "Result prePreMax: " + masResult.get(2));
+    }
+}
